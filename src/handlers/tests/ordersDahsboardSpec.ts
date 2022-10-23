468baa1d1b345d4df0e7d76f4d918ab60f5267b6
tarek-elmasry <dr.tareqelmasry@hotmail.com>
2022-10-23 12:50:31 +0300

    refactor: tests to use tokens from a login process

--- src/handlers/tests/ordersDahsboardSpec.ts
+++ src/handlers/tests/ordersDahsboardSpec.ts
@@ -1,10 +1,14 @@
 import supertest from 'supertest';
 import { Order } from '../../models/order';
 import { app } from '../../server';
-import { jwtSign } from '../../utils/jwt_tokens';
 
 const request = supertest(app);
-const userToken = jwtSign({ id: 1, username: 'leomessi' });
+
+const getToken = async (): Promise<string> => {
+  const credentials = { username: 'leomessi', password: '12345' };
+  const res = await request.post('/users/auth').send(credentials);
+  return res.body.token;
+};
 
 describe('/order-services endpoint tests', () => {
   const endpoint = '/order-services/orders-by-user-id';
@@ -16,7 +20,7 @@ describe('/order-services endpoint tests', () => {
     it('200 response with array of orders', async () => {
       const res = await request
         .get(endpoint + '/1')
-        .set('Authorization', 'Bearer ' + userToken);
+        .set('Authorization', 'Bearer ' + (await getToken()));
 
       expect(res.body instanceof Array).toBeTrue();
       expect(res.status).toBe(200);
@@ -25,7 +29,7 @@ describe('/order-services endpoint tests', () => {
     it("422 when user id doesn't exists", async () => {
       const res = await request
         .get(endpoint + '/100')
-        .set('Authorization', 'Bearer ' + userToken);
+        .set('Authorization', 'Bearer ' + (await getToken()));
 
       expect(res.status).toBe(422);
       expect(res.body.message).toEqual('No user matches id: 100');
@@ -34,7 +38,7 @@ describe('/order-services endpoint tests', () => {
     it('orders belongs to provided user_id', async () => {
       const res = await request
         .get(endpoint + '/1')
-        .set('Authorization', 'Bearer ' + userToken);
+        .set('Authorization', 'Bearer ' + (await getToken()));
 
       const orders = res.body;
       orders.forEach((order: Order) => {
@@ -52,7 +56,7 @@ describe('/order-services endpoint tests', () => {
     it('200 and order status is active', async () => {
       const res = await request
         .get(endpoint + '/1/active')
-        .set('Authorization', 'Bearer ' + userToken);
+        .set('Authorization', 'Bearer ' + (await getToken()));
 
       expect(res.status).toBe(200);
       expect(res.body.status).toBe('active');
@@ -68,7 +72,7 @@ describe('/order-services endpoint tests', () => {
     it('200 and array of orders with status is completed', async () => {
       const res = await request
         .get(endpoint + '/1/completed')
-        .set('Authorization', 'Bearer ' + userToken);
+        .set('Authorization', 'Bearer ' + (await getToken()));
 
       const orders = res.body;
       expect(res.status).toBe(200);
