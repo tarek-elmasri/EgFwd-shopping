5f9b15cfe0d89e891905e5c137a97157a88081a2
tarek-elmasry <dr.tareqelmasry@hotmail.com>
2022-10-23 12:58:55 +0300

    test: refactor addProduct routes to use auth headers

--- src/handlers/tests/ordersSpec.ts
+++ src/handlers/tests/ordersSpec.ts
@@ -3,10 +3,22 @@ import { app } from '../../server';
 
 const request = supertest(app);
 
+const getToken = async (): Promise<string> => {
+  const credentials = { username: 'leomessi', password: '12345' };
+  const res = await request.post('/users/auth').send(credentials);
+  return res.body.token;
+};
+
 describe('/orders endpoints handler tests', () => {
+  it('/:order_id/products [POST] 401 with invalid auth headers', async () => {
+    const res = await request.post('/orders/3/products');
+    expect(res.status).toBe(401);
+  });
+
   it('/:order_id/products [POST] 201 status', async () => {
     const res = await request
       .post('/orders/3/products')
+      .set('Authorization', 'Bearer ' + (await getToken()))
       .send({ product_id: 1, quantity: 10 });
 
     expect(res.statusCode).toBe(201);
@@ -18,6 +30,7 @@ describe('/orders endpoints handler tests', () => {
   it('/:order_id/products [POST] 400 status with invalid order_id params', async () => {
     const res = await request
       .post('/orders/cc/products')
+      .set('Authorization', 'Bearer ' + (await getToken()))
       .send({ product_id: 1, quantity: 10 });
 
     expect(res.status).toEqual(400);
@@ -26,6 +39,7 @@ describe('/orders endpoints handler tests', () => {
   it("/:order_id/products [POST] 422 when product deosn't exist", async () => {
     const res = await request
       .post('/orders/3/products')
+      .set('Authorization', 'Bearer ' + (await getToken()))
       .send({ product_id: 11, quantity: 10 });
 
     expect(res.status).toBe(422);
