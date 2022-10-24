038283e05a04e8910a1a60116d3500a14617ae74
tarek-elmasry <dr.tareqelmasry@hotmail.com>
2022-10-24 13:02:00 +0300

    fix: database port dynamically from environment

--- src/database.ts
+++ src/database.ts
@@ -6,14 +6,16 @@ let client: Pool;
 
 if (process.env.ENV === 'dev') {
   client = new Pool({
-    host: process.env.DB_HOST,
+    host: process.env.DB_HOST_DEV,
     database: process.env.DB_NAME,
+    port: parseInt(process.env.DB_PORT_DEV as string) || 5432,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
   });
 } else {
   client = new Pool({
-    host: process.env.DB_HOST,
+    host: process.env.DB_HOST_TEST,
+    port: parseInt(process.env.DB_PORT_TEST as string) || 5432,
     database: process.env.DB_NAME_TEST,
     user: process.env.DB_USER_TEST,
     password: process.env.DB_PASSWORD_TEST,
